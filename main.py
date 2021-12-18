68fb15d0bbad1421f4045ca64b3a63396c929603
✗ BᵣₐyDₑₙ ✗ <75217387+BrayDanXD@users.noreply.github.com>
2021-12-18 20:37:58 +0530

    TeamOctave

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from pytgcalls import idle
+
+from config import call_py
+from Userbot.quote import arq
+
+
+async def main():
+    await call_py.start()
+    print(
+        """
+    ------------------
+   | Userbot Started! POWERED BY @TeamOctave |
+    ------------------
+"""
+    )
+    await call_py.join_chat(GROUP_ID)
+    await call_py.send_message(GROUP_ID, "I Used Your Code For Music")
+    await idle()
+    await arq.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
